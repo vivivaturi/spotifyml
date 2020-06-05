420a729063fba7ffd02d5e5257b4718cee9a1bec
vivivaturi <55408226+vivivaturi@users.noreply.github.com>
2020-06-04 21:11:22 -0400

    Outlining

--- spotify.py
+++ spotify.py
@@ -4,6 +4,9 @@ Module for model that predicts which song I will like using ML
 
 class spotibot:
     """
-    Class that interfaces with the spotify API
+    Class that interfaces with the spotify API.
+
+    This class will analayze a playlist to determine what songs will be liked
+    using information gathered from the Spotify API. This will eventually interface
+    with a discord bot.
     """
-    
